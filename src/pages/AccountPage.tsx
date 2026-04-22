318a0335583b4b58e2b3a2bf616a10aa7c0b27c1
tdaprod-maker <tdaprod@gmail.com>
2026-04-22 12:08:40 -0300

    fix: remove import não utilizado no AccountPage

--- src/pages/AccountPage.tsx
+++ src/pages/AccountPage.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react'
 import { supabase } from '../lib/supabase'
-import { getTokenBalance } from '../services/tokens'
 import { loadBrandConfig } from '../services/brandKit'
 
 export function AccountPage() {
